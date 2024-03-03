bf20c31a08b2f33e5b9c65c7901a009d79a76db1
unknown <georgejosephhamilton@gmail.com>
2024-03-02 23:42:04 -0500

    Test sort_rankings
    Add test cases for the sort_rankings() pure function.

---
+++ tests/test_sort_rankings.py
@@ -0,0 +1,29 @@
+# Test check for active files
+from datetime import datetime, timezone
+import pytest
+from linksiren.pure_functions import sort_rankings
+
+def test_empty():
+    folder_rankings = {}
+    expected_result = {}
+    assert sort_rankings(folder_rankings) == expected_result
+
+def test_single():
+    folder_rankings = {'Folder1': 10}
+    expected_result = {'Folder1': 10}
+    assert sort_rankings(folder_rankings) == expected_result
+
+def test_multiple_unique():
+    folder_rankings = {'Folder1': 10, 'Folder2': 5, 'Folder3': 8}
+    expected_result = {'Folder1': 10, 'Folder3': 8, 'Folder2': 5}
+    assert sort_rankings(folder_rankings) == expected_result
+
+def test_multiple_duplicate():
+    folder_rankings = {'Folder1': 10, 'Folder2': 5, 'Folder3': 10}
+    expected_result = {'Folder1': 10, 'Folder3': 10, 'Folder2': 5}
+    assert sort_rankings(folder_rankings) == expected_result
+
+def test_multiple_negatives():
+    folder_rankings = {'Folder1': -10, 'Folder2': -5, 'Folder3': -8}
+    expected_result = {'Folder2': -5, 'Folder3': -8, 'Folder1': -10}
+    assert sort_rankings(folder_rankings) == expected_result
