8c2b54877953942eed0ecea3f8c442aac39f3b7b
gjhami <gjhami@users.noreply.github.com>
2024-10-28 11:34:01 -0400

    Main PEP Compliance
    Make __main.py__ PEP compliant.

--- src/linksiren/__main__.py
+++ src/linksiren/__main__.py
@@ -1,11 +1,37 @@
-import json
-from datetime import datetime
-from pathlib import Path
+'''
+Author: George Hamilton
+Main module for the LinkSiren application.
+This module handles different modes of operation for the LinkSiren application. It parses
+command-line arguments and executes the appropriate handler based on the specified mode.
+The available modes are:
+If credentials are provided in the arguments, they are parsed and used in the appropriate handlers.
+Functions:
+    main(): Main function to handle different modes of operation for the LinkSiren application.
+Usage:
+    Run this module as a script to execute the main function.
+'''
 from impacket.examples.utils import parse_credentials
 from linksiren.arg_parser import parse_args
-from linksiren.mode_handlers import handle_generate, handle_rank, handle_identify, handle_deploy, handle_cleanup
+from linksiren.mode_handlers import handle_generate, handle_rank, handle_identify, \
+                                    handle_deploy, handle_cleanup
 
 def main():
+    """
+    Main function to handle different modes of operation for the LinkSiren application.
+    This function parses command-line arguments and executes the appropriate handler
+    based on the specified mode. The available modes are:
+    - 'generate': Generates necessary data or configurations.
+    - 'rank': Ranks items based on specified criteria.
+    - 'identify': Identifies specific elements or patterns.
+    - 'deploy': Deploys the application or its components.
+    - 'cleanup': Cleans up resources or temporary data.
+    If credentials are provided in the arguments, they are parsed and used in the
+    appropriate handlers.
+    Args:
+        None
+    Returns:
+        None
+    """
     args = parse_args()
     if 'credentials' in args:
         domain, username, password = parse_credentials(args.credentials)
